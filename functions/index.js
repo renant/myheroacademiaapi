08193f21d50e2742faf4b72c69de8da1f305aeb0
Renan Teixeira <renan.replay@live.com>
2020-09-17 17:59:19 -0300

    add more properties to character

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const admin = require('firebase-admin');
+const { PubSub } = require('@google-cloud/pubsub');
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -10,8 +11,10 @@ const baseUrl = 'https://bokunoheroacademia.fandom.com/wiki';
 const urlBaseCharacters = () => `${baseUrl}/List_of_Characters`;
 const urlBaseCharacter = (id) => `${baseUrl}/${id}`;
 
+
+
 exports.getCharacters = functions
-  .runWith({ memory: '1GB' })
+  .runWith({ memory: '1GB', timeoutSeconds: 120 })
   .https.onRequest(async (request, response) => {
 
     const browser = await puppeteer.launch({
@@ -31,19 +34,22 @@ exports.getCharacters = functions
     })
 
 
+    const idsList = [];
+
     listHtml.forEach(e => {
       const $ = cheerio.load(e);
 
       const image = $('img').attr('data-src');
-      const name = $('.chargallery-profile-caption').text();
       const id = $('a').attr('href');
 
+
+
       const character = {
-        name,
         id: id.replace('/wiki/', ''),
-        image
+        images: [image]
       };
 
+      idsList.push(character.id);
       const docRef = db.collection('characters').doc(character.id);
 
       docRef.set(character);
@@ -56,72 +62,130 @@ exports.getCharacters = functions
     //   characters.push(e.data());
     // })
 
+    const pubSub = new PubSub({
+      projectId: process.env.GCLOUD_PROJECT
+    })
+
+    const publishPromises = idsList.map(id => {
+      return pubSub.topic(fetchCharactersTopic).publishJSON({ id });
+    })
+
+    await Promise.all(publishPromises);
     response.status(200).send('ok');
   });
 
-
+const fetchCharactersTopic = 'fetch-characters-topic';
 exports.getCharacter = functions
   .runWith({ memory: '1GB' })
   .https.onRequest(async (request, response) => {
+    //.pubsub.topic(fetchCharactersTopic)
+    //.onPublish(async (msg) => {
+    try {
+      //const { id } = msg.json
+      const { id } = request.query;
+      const characterRef = await db.collection("characters").doc(id).get();
 
-    const { id } = request.query;
-    if (!id) {
-      response.status(400).json({ 'message': 'Missing id query' })
-      return;
-    }
+      const character = characterRef.data();
 
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto(urlBaseCharacter(id));
+      if (!character) {
+        response.status(404).json({ 'message': 'Character not found' });
+        return;
+      }
 
-    const html = await page.evaluate(() => {
-      return document.querySelector('.mw-content-text').innerHTML
-    })
+      const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
+      const page = await browser.newPage();
+      await page.goto(urlBaseCharacter(id));
+
+      const html = await page.evaluate(() => {
+        return document.querySelector('.mw-content-text').innerHTML
+      })
+
+      const $ = cheerio.load(html);
+
+      character.name = $('aside > h2').text() || null;
+
+      const charactersImages = [];
+
+      $('aside > section').each((_, e) => {
+        $(e).find('div').each((_, info) => {
+          const value = $(info).attr('data-source') || null;
+          if (value) {
+            if (value === 'gender') {
+              character[value] = $(info).find('img').attr('alt') || null
+            }
+            else if (value === 'family') {
+              character[value] = $(info).find('a').map((_, familyMember) => {
+                const familyMemberId = $(familyMember).attr('href') || null
+                const familyMemberName = $(familyMember).text() || null
+
+                return {
+                  id: familyMemberId.replace('/wiki/', ''),
+                  name: familyMemberName
+                }
+              }).toArray();
+            }
+            else if (value === 'age') {
+              const age = $(info).find('div').text();
+              if (age.includes(')')) {
+                character.ages = age.split(')').filter(e => e !== '').map(e => { return { age: e.split('(')[0], when: e.split('(')[1] } });
+              }
+            }
+            else if (value === 'quirk') {
+
+              if ($(info).find('div').contents().length > 1) {
+                character[value] = $(info).find('div').contents().map((_, quirk) => {
+                  return $(quirk).contents().first().text() || null;
+                }).toArray().join(', ');
+              } else {
+                character[value] = $(info).find('div').text();
+              }
 
 
-    const $ = cheerio.load(html);
-
-    const character = {
-      id,
-      name: $('aside > h2').text() || null,
-      images: [],
-    }
-
-
-    $('aside > section').each((_, e) => {
-      //get attributes
-      $(e).find('div').each((_, info) => {
-        const value = $(info).attr('data-source') || null;
-        if (value) {
-          if (value === 'gender') {
-
-            character[value] = $(info).find('img').attr('alt') || null
-          } else {
-            character[value] = $(info).find('div').contents().first().text() || null;
+            }
+            else if (value === 'occupation') {
+              character[value] = $(info).find('div').contents().map((_, occupation) => {
+                return $(occupation).text();
+              }).toArray().filter(e => !e.includes('(')).join(', ').replace(', , ', ', ');
+            }
+            else if (value === 'teams') {
+              character[value] = $(info).find('div').contents().map((_, team) => {
+                return $(team).text();
+              }).toArray().join(', ').replace(', , ', ', ');
+            }
+            else if (value.includes('voice')) {
+              return;
+            }
+            else {
+              character[value] = $(info).find('div').contents().first().text() || null;
+            }
           }
-        }
+        })
+      });
+
+      $('aside').each((_, e) => {
+        $(e).find('.image-thumbnail').each((_, image) => {
+          const findImage = $(image).attr('href')
+          if (!findImage.includes('/wiki/')) {
+            charactersImages.push(findImage);
+          }
+        })
       })
 
+      character.description = ($('nav').prevAll('p').map((_, description) => {
+        return $(description).contents().text().replace(/\[.*?\]/, '')
+      }).toArray().reverse().join(''));
 
+      const uniqueImages = new Set(charactersImages.concat(character.images))
+      character.images = [...uniqueImages];
 
-    });
+      var docRef = db.collection("characters").doc(id);
 
-
-    $('aside').each((_, e) => {
-      $(e).find('.image-thumbnail').each((_, image) => {
-        const findImage = $(image).attr('href')
-
-        if (!findImage.includes('/wiki/')) {
-          character.images.push(findImage);
-        }
-
-      })
-    })
-
-
-
-    response.status(200).json(character);
+      docRef.update(character);
+      response.status(200).json(character);
+    } catch (error) {
+      console.error(error)
+    }
   });
